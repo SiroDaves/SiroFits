2d3eb95b446e50390153982ef9220eae08ec4f77
Siro Daves <sirodevske@gmail.com>
2024-04-05 10:00:00 +0300

    Changes in header

--- components/reusable/Header.tsx
+++ components/reusable/Header.tsx
@@ -8,8 +8,8 @@ export const Header: FC<HeaderProps> = () => {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-orange-500 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
-                <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" 
-                viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
+                <svg className="fill-current h-8 w-8 mr-2" width="54" height="54"
+                    viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
                 <span className="font-semibold text-xl tracking-tight">Siro Fits</span>
             </div>
             <div className="block lg:hidden">
@@ -19,18 +19,18 @@ export const Header: FC<HeaderProps> = () => {
             </div>
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div className="text-sm lg:flex-grow">
-                    <a href="#" 
-                    className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4"
+                    <a href="#"
+                        className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4"
                     >
                         GPX
                     </a>
-                    <a href="#" 
-                    className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4"
+                    <a href="#"
+                        className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4"
                     >
                         FIT
                     </a>
-                    <a href="#" 
-                    className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4"
+                    <a href="#"
+                        className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4"
                     >
                         TCX
                     </a>
